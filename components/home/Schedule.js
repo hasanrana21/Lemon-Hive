c8042252da027624325e14fc1473bdd850eba799
hasanrana21 <hasanrana178821@gmail.com>
2022-10-25 00:09:51 +0600

    created table with dynamic data

--- components/home/Schedule.js
+++ components/home/Schedule.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/Home.module.css";
 import axios from "axios";
+// import Link from "next/link";
 
 const Schedule = () => {
-  const [conferences, setConferences] = useState([]);
+  const [datas, setDatas] = useState([]);
+  const [maxData, setMaxData] = useState(0);
+
   const conferencesQuery = `
     query{
         conferences{
           id,
           name,
           startDate,
+          year,
           schedules{
             day,
+            description,
+            location{
+              address,
+              city,
+              country{
+                name
+              }
+            }
             intervals{
               begin,
               end
@@ -26,13 +38,24 @@ const Schedule = () => {
         query: conferencesQuery,
       })
       .then((res) => {
-        setConferences(res.data.data.conferences);
+        const allConferences = res.data.data.conferences;
+        const allData = [];
+        const maxData = [];
+        allConferences.map((conference) => {
+          conference.schedules.map((item) => {
+            allData.push(item);
+            console.log(item);
+            maxData.push(item.intervals.length);
+          });
+        });
+        setMaxData(Math.max(...maxData));
+        setDatas(allData);
       });
   };
   useEffect(() => {
     fetchConferences();
   }, []);
-  console.log("conferences", conferences);
+  // console.log("maxData", maxData);
   return (
     <div className="p-7">
       <h3 className="text-4xl font-medium mb-6">Event Schedule</h3>
@@ -42,28 +65,34 @@ const Schedule = () => {
       </p>
       <table className="min-w-full mt-14">
         <thead>
-          <tr>
+          <tr className={styles.tr}>
             <th className={styles.th}></th>
-            <th className={styles.th}>Mon</th>
-            <th className={styles.th}>Tue</th>
-            <th className={styles.th}>Wed</th>
-            <th className={styles.th}>Thu</th>
-            <th className={styles.th}>Fri</th>
-            <th className={styles.th}>Sat</th>
-            <th className={styles.th}>Sun</th>
+            {[...Array(maxData)].map((item, key) => (
+              <th key={key} className={styles.th}>
+                Session {key + 1}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className={styles.td}>asfda</td>
-            <td className={styles.td}>asdfa</td>
-            <td className={styles.td}>asdfasdf</td>
-            <td className={styles.td}>asdfasdf</td>
-            <td className={styles.td}>asdfasdf</td>
-            <td className={styles.td}>asdfasdf</td>
-            <td className={styles.td}>asdfasdf</td>
-            <td className={styles.td}>asdfasdf</td>
-          </tr>
+          {datas.map((data, key) => (
+            <tr key={key} className={styles.tr}>
+              <td className={styles.td}>{data?.day}</td>
+              {data.intervals.map((interval, index) => (
+                <td key={index} className={styles.td}>
+                  <div className="border border-primary-1 px-4 py-2 rounded bg-primary-1/10">
+                    {/* {data.location?.address}, &nbsp;
+                    {data.location?.city}, &nbsp;
+                    {data.location?.country?.name} */}
+                    {data?.description}
+                    <p>
+                      {interval?.begin}--{interval?.end}
+                    </p>
+                  </div>
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
